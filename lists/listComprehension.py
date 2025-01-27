3591590ac28b79439b546593fb9dcdad7be1c736
altosha228 <100281604+altosha228@users.noreply.github.com>
2025-01-27 16:14:16 +0500

    Delete lists directory

--- lists/listComprehension.py
+++
@@ -1,7 +0,0 @@
-list = ["gpu", "cpu", "motherboard", "Hard Drive", "SATA SSD", "NVMe SSD", "RAM pcb"]
-
-newList = [item.upper() for item in list if "u" in item]
-print(newList)
-
-newList = ["yo" for item in list]
-print(newList)
