432b2790bc15f2b6e47add9dfdc5f203be85a1f4
altosha228 <100281604+altosha228@users.noreply.github.com>
2025-01-27 16:14:28 +0500

    Delete sets directory

--- sets/sets.py
+++
@@ -1,4 +0,0 @@
-u = {"Apple", "Refridgerator", 4, False}
-
-#set is unordered, distinct collection containgin different datatypes
-#the differnce from tuple is that you can add or remove items but not change them
